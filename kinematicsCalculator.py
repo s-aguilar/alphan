0de898a87e16c71565c2dfbf425f499aab8eea8e
s-aguilar <33613476+s-aguilar@users.noreply.github.com>
2018-08-20 09:26:51 -0400

    QoL updates
    Implemented 3D plotting. Ability to compare 10C scattering with 10B and 11B.

--- kinematicsCalculator.py
+++ kinematicsCalculator.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-#from Analyzor.KINEMATICS.tbjcconstants import *
+from Analyzor.KINEMATICS.AtomicMassTable import GetElement
 
 
 # Constants
 _c = 3.0e8
 _U = 931.4940954 #MeV/c^2
-_m4He = 4.00260305404663*_U
-_m10C = 10.0168533325195*_U
+_m4He = GetElement(2,4)[3]*_U
+_m10B = GetElement(5,10)[3]*_U
+_m11B = GetElement(5,11)[3]*_U
+_m10C = GetElement(6,10)[3]*_U
+
 
 def classicalEnergyToMomentum(energy,mass):
     """ unit: energy(MeV), mass(MeV/c^2)"""
@@ -34,7 +37,7 @@ def energyToAngleAndEnergy(e_vertex,e1,mass1,ex_energy1=0,ex_energy2=0):
 
     # Conserve energy
     deltaE = ex_energy1 + ex_energy2
-    e2 = e_vertex - e1 - deltaE # ex_energy term added
+    e2 = e_vertex - e1 - deltaE
 
     # Sort
     if mass1 == _m4He:
@@ -45,8 +48,8 @@ def energyToAngleAndEnergy(e_vertex,e1,mass1,ex_energy1=0,ex_energy2=0):
         raise Exception("It is neither a helium nor a carbon")
 
     # Assign momentums
-    mom1 = classicalEnergyToMomentum(e1,mass1) # ex_energy term added
-    mom2 = classicalEnergyToMomentum(e2,mass2) # ex_energy term added
+    mom1 = classicalEnergyToMomentum(e1,mass1)
+    mom2 = classicalEnergyToMomentum(e2,mass2)
 
     # Apply conservation of momentum
     mom0 = classicalEnergyToMomentum(e_vertex,_m10C)
@@ -142,43 +145,31 @@ def generalAngleToAngleEnergy(e_vertex,mass_vertex,theta_1,mass_1,mass_2):
     to find energy and angle of outgoing
     """
 
-    massb = mass_vertex # this one changes
-
-    mass1 = mass_1
-    mass2 = mass_2
-
-    # if mass_1 == _m4He:
-    #     mass1 = mass_1
-    #     mass2 = _m10C
-    # elif mass_1 == _m10C:
-    #     mass1 = mass_1
-    #     mass2 = _m4He
-
     theta1 = theta_1 * np.pi/180
 
     # Apply conservation of momentum
     mom0 = classicalEnergyToMomentum(e_vertex,_m10C)
 
     # 2 solutions from quadratic equation
-    mom1_1,mom1_2 = quadraticSolver(1+mass2/mass1,-2*mom0*np.cos(theta1),-mom0*mom0*(mass2/massb-1))
-    mom2_1 = np.sqrt(2*mass2*(mom0*mom0/2/massb-mom1_1*mom1_1/2/mass1))
-    mom2_2 = np.sqrt(2*mass2*(mom0*mom0/2/massb-mom1_2*mom1_2/2/mass1))
+    mom1_1,mom1_2 = quadraticSolver(1+mass_2/mass_1,-2*mom0*np.cos(theta1),-mom0*mom0*(mass_2/mass_vertex-1))
+    mom2_1 = np.sqrt(2*mass_2*(mom0*mom0/2/mass_vertex-mom1_1*mom1_1/2/mass_1))
+    mom2_2 = np.sqrt(2*mass_2*(mom0*mom0/2/mass_vertex-mom1_2*mom1_2/2/mass_1))
 
     # Assign energies
-    e1_1 = mom1_1*mom1_1/2/mass1
-    e1_2 = mom1_2*mom1_2/2/mass1
-    e2_1 = mom2_1*mom2_1/2/mass2
-    e2_2 = mom2_2*mom2_2/2/mass2
+    e1_1 = mom1_1*mom1_1/2/mass_1
+    e1_2 = mom1_2*mom1_2/2/mass_1
+    e2_1 = mom2_1*mom2_1/2/mass_2
+    e2_2 = mom2_2*mom2_2/2/mass_2
 
     # Find other angle
     theta2_1 = np.arcsin(mom1_1*np.sin(theta1)/mom2_1) * 180/np.pi
     theta2_2 = np.arcsin(mom1_2*np.sin(theta1)/mom2_2) * 180/np.pi
 
+    # Concatenate both solutions from quadratic equation
     e1 = np.concatenate((e1_1,e1_2))
     e2 = np.concatenate((e2_1,e2_2))
     theta1 = np.concatenate((theta1,theta1))
     theta2 = np.concatenate((theta2_1,theta2_2))
-
     return e1,theta1,e2,theta2
 
 """
@@ -186,61 +177,69 @@ def generalAngleToAngleEnergy(e_vertex,mass_vertex,theta_1,mass_1,mass_2):
 ## MAIN ##
 ##########
 """
-#ev = 15*np.ones(91)
-#e1 = np.linspace(0,15,91)
-ang1 = np.linspace(0,90,91)
 
-# e1,ang1,e2,ang2 = energyToAngleAndEnergy(ev,e1,_m4He,ex_energy1=0,ex_energy2=0)
-# e1,ang1,e2,ang2 = angleToAngleEnergy1(ev,ang1,0,1)
-#e1,ang1,e2,ang2 = angleToAngleEnergy2(ev,ang1,_m10C)
+# Initialize arrays for 10C,11B,10B
+e1_plot,ang1_plot,e2_plot,ang2_plot = np.array([]),np.array([]),np.array([]),np.array([])
+_e1_plot,_ang1_plot,_e2_plot,_ang2_plot = np.array([]),np.array([]),np.array([]),np.array([])
+__e1_plot,__ang1_plot,__e2_plot,__ang2_plot = np.array([]),np.array([]),np.array([]),np.array([])
 
-e1_plot,ang1_plot,e2_plot,ang2_plot = [],[],[],[]
-_e1_plot,_ang1_plot,_e2_plot,_ang2_plot = [],[],[],[]
-
-erange = np.linspace(34,35,100)
+erange = np.linspace(34,35,3)
 
 for iii in erange:
-    # rest input angle array after each iteration
-    ang1 = np.linspace(0,90,91)
-    _ang1 = np.linspace(0,90,91)
-    ev = (1+iii)*np.ones(91)
+    # reset input angle array after each iteration
+    ang1, _ang1, __ang1 = np.linspace(0,90,91),np.linspace(0,90,91),np.linspace(0,90,91)
+
+    ev = iii*np.ones(91)
+
+    # 10C
     e1,ang1,e2,ang2 = generalAngleToAngleEnergy(ev,_m10C,ang1,_m4He,_m10C)
-    # this second plot will be used to see if curves are separate
-    _e1,_ang1,_e2,_ang2 = generalAngleToAngleEnergy(ev,_m10C,_ang1,_m4He,_m10C)
-    e1_plot.append(e1)
-    ang1_plot.append(ang1)
-    e2_plot.append(e2)
-    ang2_plot.append(ang2)
-    _e1_plot.append(_e1)
-    _ang1_plot.append(_ang1)
-    _e2_plot.append(_e2)
-    _ang2_plot.append(_ang2)
+    e1_plot = np.append(e1_plot,e1)
+    ang1_plot = np.append(ang1_plot,ang1)
+    e2_plot = np.append(e2_plot,e2)
+    ang2_plot = np.append(ang2_plot,ang2)
 
-# 3d plot
+    # 11B
+    _e1,_ang1,_e2,_ang2 = generalAngleToAngleEnergy(ev,_m11B,_ang1,_m4He,_m11B)
+    _e1_plot = np.append(_e1_plot,_e1)
+    _ang1_plot = np.append(_ang1_plot,_ang1)
+    _e2_plot = np.append(_e2_plot,_e2)
+    _ang2_plot = np.append(_ang2_plot,_ang2)
+
+    # 10B
+    __e1,__ang1,__e2,__ang2 = generalAngleToAngleEnergy(ev,_m10B,__ang1,_m4He,_m10B)
+    __e1_plot = np.append(__e1_plot,__e1)
+    __ang1_plot = np.append(__ang1_plot,__ang1)
+    __e2_plot = np.append(__e2_plot,__e2)
+    __ang2_plot = np.append(__ang2_plot,__ang2)
+
+
+# 3D surface plot
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(e1_plot,e2_plot,ang2_plot)
-ax.scatter(_e1_plot,_e2_plot,_ang2_plot)
+ax = fig.gca(projection='3d')
+ax.plot_trisurf(e1_plot,e2_plot,ang2_plot,color='b', alpha=0.8)#,label=r'$^{10}C(\alpha,\alpha)^{10}C$')
+# ax.plot(_e1_plot,_e2_plot,_ang2_plot,color='r', alpha=0.8,label=r'$^{11}B(\alpha,\alpha)^{11}B$')
+# ax.plot(__e1_plot,__e2_plot,__ang2_plot,color='y', alpha=0.8,label=r'$^{10}B(\alpha,\alpha)^{10}B$')
 
-ax.set_xlabel('e1')
-ax.set_ylabel('e2')
-ax.set_zlabel(r'$\ttheta_{2}')
+ax.set_xlabel("e1")
+ax.set_ylabel("e2")
+ax.set_zlabel(r"$\theta_{2}$")
+ax.legend()
 plt.show()
 
 
-# plt.scatter(ang1,ang2)
+# plt.scatter(_ang1,_ang2)
 # plt.title(r"$\theta_{2}$ vs $\theta_{1}$")
 # plt.xlabel(r"$\theta_{1} [deg]$")
 # plt.ylabel(r"$\theta_{2} [deg]$")
 # plt.show()
 #
-# plt.scatter(ang1,e1)
+# plt.scatter(_ang1,_e1)
 # plt.title(r"$E_{1}$ vs $\theta_{1}$")
 # plt.xlabel(r"$\theta_{1} [deg]$")
 # plt.ylabel("E$_{1}$ [MeV]")
 # plt.show()
 #
-# plt.scatter(ang2,e2)
+# plt.scatter(_ang2,_e2)
 # plt.title(r"$E_{2}$ vs $\theta_{2}$")
 # plt.xlabel(r"$\theta_{2} [deg]$")
 # plt.ylabel("E$_{2}$ [MeV]")
